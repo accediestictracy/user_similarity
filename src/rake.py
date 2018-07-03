49eb19e937e40927153539dc4ce5f8770e71ca42
Kacper Radzikowski <radzikowskikacper@gmail.com>
2018-07-03 22:21:05 +0900

    saving pprocessed

--- src/rake.py
+++ src/rake.py
@@ -6,6 +6,8 @@ import enchant
 import pandas as pd
 import RAKE
 
+# from sentiment_score.sentiment import sentiment_score
+
 
 def filter(tweet, dictionary):
     print(tweet)
@@ -41,30 +43,34 @@ def process_tweets(directory):
     label2vec = word2vec('../data/glove_twitter_27B_25d.txt')
     if not os.path.isdir('../data/labels/'):
         os.makedirs('../data/labels/')
+    if not os.path.isdir('../data/preprocessed/'):
+        os.makedirs('../data/preprocessed/')
     for file in sorted(os.listdir(directory)):
         if file.endswith(".csv"):
             df = pd.read_csv(f'../data/tweets/{file}', header=0, encoding="utf-8")
             vectorlist = []
-            for index, tweet in enumerate(df['text']):
-                filtered = filter(tweet, dictionary)
-                if len(filtered):
-                    labels = rake.run(' '.join(filtered))
-                    if len(labels):
-                        for keyword in labels[0][0].split():
-                            try:
-                                vector = label2vec[keyword]
-                                print(filtered)
-                                print(labels)
-                                print(keyword)
-                                to_add_vector = [df['tweet_id'][index]] + vector
-                                vectorlist.append(to_add_vector)
-                                break
-                            except KeyError:
-                                pass
+            with open(f'../data/preprocessed/{file}', 'w') as file_handler:
+                for index, tweet in enumerate(df['text']):
+                    filtered = filter(tweet, dictionary)
+                    if len(filtered):
+                        labels = rake.run(' '.join(filtered))
+                        if len(labels):
+                            for keyword in labels[0][0].split():
+                                try:
+                                    vector = label2vec[keyword]
+                                    print(filtered)
+                                    print(labels)
+                                    print(keyword)
+                                    to_add_vector = [df['tweet_id'][index]] + vector
+                                    vectorlist.append(to_add_vector)
+                                    break
+                                except KeyError:
+                                    pass
+                    file_handler.write(' '.join(filtered))
 
-            with open(f'../data/labels/{file.replace("tweets", "label2v")}', 'w') as f:
-                writer = csv.writer(f)
-                writer.writerows(vectorlist)
+                with open(f'../data/labels/{file.replace("tweets", "label2v")}', 'w') as f:
+                    writer = csv.writer(f)
+                    writer.writerows(vectorlist)
 
 
 if __name__ == '__main__':
